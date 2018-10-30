c075616386684e621b54d757ab3b1b4cb0cbbf2d
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2018-10-29 23:39:06 -0400

    signin and out works, register succesfully creates user, but throws error re-rendering to home screen

--- public-client/src/App.js
+++ public-client/src/App.js
@@ -10,20 +10,27 @@ import {
   // NavbarHeading,
   // Switch
 } from "@blueprintjs/core";
-import Home from "./Components/screens/home.js";
+import Home from "./Components/screens/home";
+import Login from "./Components/screens/login";
 import "./App.css";
 import axios from "axios";
+import TokenService from "./Services/tokenService";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       unfilteredFeatureList: [],
-      count: ""
+      count: "",
+      isLoggedIn: false,
+      user: []
     };
     this.getAllFeatures = this.getAllFeatures.bind(this);
     this.editFeature = this.editFeature.bind(this);
     // this.getVotes = this.getVotes.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.register = this.register.bind(this);
   }
 
   getAllFeatures() {
@@ -80,6 +87,48 @@ class App extends Component {
       .catch(err => console.log(`editFeature err: ${err}`));
   }
 
+  login(email, password) {
+    // console.log("login app.js", data);
+    axios({
+      url: "http://localhost:8080/users/login",
+      method: "POST",
+      data: { email: email, password: password }
+    })
+      .then(response => {
+        TokenService.save(response.data.token);
+        console.log("login response", response.data);
+        this.setState({ isLoggedIn: true, user: response.data.user });
+      })
+      .catch(err => console.log("login err", err));
+  }
+
+  register(email, password) {
+    // console.log("register app.js", data);
+    this.setState({ isLoggedIn: "WAITING" });
+    axios({
+      url: "http://localhost:8080/users",
+      method: "POST",
+      data: { email: email, password: password }
+    })
+      .then(response => {
+        TokenService.save(response.data.token);
+        console.log("register response", response.data);
+        this.setState({ isLoggedIn: true, user: response.data.user });
+      })
+      .catch(err => console.log("register err", err));
+  }
+
+  logout() {
+    TokenService.destroy();
+    this.setState({
+      isLoggedIn: false,
+      user: [],
+      unfilteredFeatureList: [],
+      count: ""
+    });
+    // console.log("logged out user?", this.state.user);
+  }
+
   // getVotes(feature_id) {
   //   axios({
   //     url: `http://localhost:8080/activities/votes/${feature_id}`
@@ -100,24 +149,33 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <Navbar>
-          <Navbar.Group align={Alignment.LEFT}>
-            <Navbar.Heading>Blueprint</Navbar.Heading>
-            <Navbar.Divider />
-            <Button className="bp3-minimal" icon="home" text="Home" />
-            <Button className="bp3-minimal" icon="document" text="Files" />
-          </Navbar.Group>
-        </Navbar>
-        <Home
-          getVotes={this.getVotes}
-          editFeature={this.editFeature}
-          unfilteredFeatureList={this.state.unfilteredFeatureList}
-          // votes={this.state.votes}
-        />
-      </div>
-    );
+    if (this.state.isLoggedIn === true) {
+      return (
+        <div>
+          <Navbar>
+            <Navbar.Group align={Alignment.LEFT}>
+              <Navbar.Heading>Blueprint</Navbar.Heading>
+              <Navbar.Divider />
+              <Button className="bp3-minimal" icon="home" text="Home" />
+              <Button className="bp3-minimal" icon="document" text="Files" />
+            </Navbar.Group>
+          </Navbar>
+          <Home
+            logout={this.logout}
+            // getVotes={this.getVotes}
+            editFeature={this.editFeature}
+            unfilteredFeatureList={this.state.unfilteredFeatureList}
+            // votes={this.state.votes}
+          />
+        </div>
+      );
+    } else if (this.state.isLoggedIn === false) {
+      return (
+        <div>
+          <Login login={this.login} register={this.register} />
+        </div>
+      );
+    }
   }
 }
 
