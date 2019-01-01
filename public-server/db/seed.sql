c12e2b677261fe1bf2d0a8db5c3d01c12e46a49a
Garrett Rasko-Martinis <garrettrm9@gmail.com>
2019-01-01 11:35:02 -0500

    fixed more bugs

--- public-server/db/seed.sql
+++ public-server/db/seed.sql
@@ -17,13 +17,13 @@ INSERT INTO products (name, company_name) VALUES
   ('Radio station', 'Spotify'),
   ('Mobile Spotify app', 'Spotify');
 
-INSERT INTO features (name, date_last_updated, product_name, user_email) VALUES
-  ('Higher quality chocolate', '2018-10-10', 'Kit Kat', 'new'),
-  ('More Kisses per bag', '2018-10-13', 'Hershey Kiss', 'test'),
-  ('Itunes UI overhaul', '2018-10-29', 'Iphone', 'new'),
-  ('Open source software', '2018-10-30', 'Apple Watch', 'test'),
-  ('Improved AI song suggestion', '2018-10-01', 'Radio station', 'new');
-  -- ('Less battery drain', '2018-10-09', 'Mobile Spotify app', 'test');
+INSERT INTO features (name, purpose, date_last_updated, product_name, user_email) VALUES
+  ('Higher quality chocolate', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-10', 'Kit Kat', 'new'),
+  ('More Kisses per bag', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-13', 'Hershey Kiss', 'test'),
+  ('Itunes UI overhaul', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-29', 'Iphone', 'new'),
+  ('Open source software', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-30', 'Apple Watch', 'test'),
+  ('Improved AI song suggestion', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-01', 'Radio station', 'new'),
+  ('Less battery drain', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-10-09', 'Mobile Spotify app', 'test');
 
 INSERT INTO activities (type, feature_id, user_email) VALUES
   ('vote', 1, 'new'),
@@ -46,13 +46,13 @@ INSERT INTO activities (type, feature_id, user_email) VALUES
   ('vote', 4, 'user1'),
   ('vote', 5, 'new'),
   ('vote', 5, 'test'),
-  -- ('vote', 6, 'new'),
+  ('vote', 6, 'new'),
   ('follow', 1, 'new'),
   ('follow', 2, 'new'),
   ('follow', 3, 'new'),
   ('follow', 4, 'test'),
-  ('follow', 5, 'test');
-  -- ('follow', 6, 'test');
+  ('follow', 5, 'test'),
+  ('follow', 6, 'test');
 
 
 -- INSERT INTO follows (feature_id, user_id) VALUES
